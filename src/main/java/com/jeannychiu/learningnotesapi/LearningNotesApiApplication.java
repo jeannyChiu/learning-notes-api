4dd443eff11674c0129be803b3dfbc418008d0dc
jeannyChiu <jeannychiu608@gmail.com>
2025-04-16 23:19:21 +0800

    Initial project setup

---
+++ src/main/java/com/jeannychiu/learningnotesapi/LearningNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeannychiu.learningnotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningNotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningNotesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeannychiu/learningnotesapi/LearningNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeannychiu.learningnotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningNotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
