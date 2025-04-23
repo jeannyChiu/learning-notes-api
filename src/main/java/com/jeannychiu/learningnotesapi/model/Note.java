49f1c61645e56fd264338887afca3aa6cbb6cc18
jeannyChiu <jeannychiu608@gmail.com>
2025-04-23 21:24:40 +0800

    feat: implement Note Entity with id, title, content, createdAt + schema auto-generation

---
+++ src/main/java/com/jeannychiu/learningnotesapi/model/Note.java
@@ -0,0 +1,53 @@
+package com.jeannychiu.learningnotesapi.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "note")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    // Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
